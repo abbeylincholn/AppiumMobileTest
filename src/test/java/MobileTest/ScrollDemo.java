70397eae40a1f034ca8e9882fc79924fe0902ddb
abbey <abbeyezekiel100@gmail.com>
2025-06-04 17:13:32 +0100

    Scroll option 2

--- src/test/java/MobileTest/ScrollDemo.java
+++ src/test/java/MobileTest/ScrollDemo.java
@@ -15,9 +15,13 @@ public class ScrollDemo extends BaseTest{
     @Test
     public void scrollDemoTest() throws MalformedURLException, URISyntaxException, InterruptedException {
         driver.findElement(AppiumBy.accessibilityId("Views")).click();
-        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))"));
+        //driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))"));
 
-
+        boolean canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "left", 100, "top", 100, "width", 200, "height", 200,
+                "direction", "down",
+                "percent", 3.0
+        ));
     }
 }
 
