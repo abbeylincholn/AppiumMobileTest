6a191c4329a37f4b73e93ad500d561aae35a8127
abbey <abbeyezekiel100@gmail.com>
2025-06-04 17:18:47 +0100

    Scroll option 2 to the end

--- src/test/java/MobileTest/ScrollDemo.java
+++ src/test/java/MobileTest/ScrollDemo.java
@@ -17,11 +17,9 @@ public class ScrollDemo extends BaseTest{
         driver.findElement(AppiumBy.accessibilityId("Views")).click();
         //driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))"));
 
-        boolean canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
-                "left", 100, "top", 100, "width", 200, "height", 200,
-                "direction", "down",
-                "percent", 3.0
-        ));
+        // scroll to the end
+        scrollToEndAction();
+
     }
 }
 
