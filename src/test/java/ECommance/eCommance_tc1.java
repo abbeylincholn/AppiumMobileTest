88a9c9328abd9bd0c4d32cd1e86d77ab4bd930d0
abbey <abbeyezekiel100@gmail.com>
2025-06-12 12:41:05 +0100

    Toast message error validation

--- src/test/java/ECommance/eCommance_tc1.java
+++ src/test/java/ECommance/eCommance_tc1.java
@@ -3,19 +3,24 @@ package ECommance;
 import MobileTest.BaseTest;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class eCommance_tc1 extends BaseTest {
 
     @Test
-    public void fillForm() {
-        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Dayo");
+    public void fillForm() throws InterruptedException {
+       // driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Dayo");
         driver.hideKeyboard();
         driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
         driver.findElement(By.id("android:id/text1")).click();
         driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"))"));
         driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getText();  // incase you have more than one toast message, you can use the index to select the specific one
+        String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+        Assert.assertEquals(toastMessage, "Please enter your name");
 
 
     }
