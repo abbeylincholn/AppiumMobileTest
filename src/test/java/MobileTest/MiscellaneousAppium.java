f147226b795e7a2a4d8e1d8eb7985fcc4170837d
abbey <abbeyezekiel100@gmail.com>
2025-06-12 10:02:29 +0100

    Landscaping functionality

---
+++ src/test/java/MobileTest/MiscellaneousAppium.java
@@ -0,0 +1,35 @@
+package MobileTest;
+
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+public class MiscellaneousAppium extends BaseTest{
+
+    @Test
+    public void WifiSettingsName() throws MalformedURLException, URISyntaxException {
+
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+
+        // set the Wifi settings name
+        driver.findElement(AppiumBy.accessibilityId("3. Preference dependencies")).click();
+        driver.findElement(AppiumBy.id("android:id/checkbox")).click();
+
+        DeviceRotation landScape = new DeviceRotation(0, 0, 90);
+        driver.rotate(landScape);
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+        String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+        Assert.assertEquals(alertTitle, "WiFi settings");
+        driver.findElement(AppiumBy.id("android:id/edit")).sendKeys("Abbey Wifi");
+        driver.findElement(AppiumBy.id("android:id/button1")).click();
+    }
+}
+
+
+
+
