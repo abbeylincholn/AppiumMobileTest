c81a1f04384eb2960d36c3793a519d3dd9eb0bf4
abbey <abbeyezekiel100@gmail.com>
2025-06-12 13:26:20 +0100

    Dynamically select product by scanning

---
+++ src/test/java/ECommance/eCommance_tc2.java
@@ -0,0 +1,47 @@
+package ECommance;
+
+import MobileTest.BaseTest;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class eCommance_tc2 extends BaseTest {
+
+    @Test
+    public void fillForm() throws InterruptedException {
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Dayo");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+        driver.findElement(By.id("android:id/text1")).click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"))"));
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Jordan 6 Rings\"))"));
+
+        int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
+        for (int i = 0; i < productCount; i++) {
+            String productName = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText();
+            if (productName.equals("Jordan 6 Rings")) {
+                driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
+                break;
+            }
+        }
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        String lastProductName = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+        Assert.assertEquals(lastProductName, "Jordan 6 Rings");
+
+
+
+    }
+}
