e8a87421ab237592e4da722eebb89f90066f3343
abbeylincholn <abbeyezekiel100@gmail.com>
2025-06-25 06:25:42 +0100

    IOS ClassChain locator

--- src/test/java/IOSMobileTest/IOSBasics.java
+++ src/test/java/IOSMobileTest/IOSBasics.java
@@ -9,6 +9,10 @@ public class IOSBasics extends IOSBaseTest {
     public void IOSBasicsTest() {
         //Xpath, classname, IOS, iosClassChain, IOSPredicateString, accessibility id, id
         driver.findElement(AppiumBy.accessibilityId("Alert Views")).click();
+        // driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name='Text Entry']")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`label == 'Text Entry'`]")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeCell")).sendKeys("Hello World");
+        driver.findElement(AppiumBy.accessibilityId("OK")).click();
     }
 
 }
